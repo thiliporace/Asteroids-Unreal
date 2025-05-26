6297c68590578432b83f38e9e0f596e06b408ea4
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-05-26 17:00:22 -0300

    comeco do sistema de input

--- Source/Asteroids/Public/PlayershipPaperCharacter.h
+++ Source/Asteroids/Public/PlayershipPaperCharacter.h
@@ -4,14 +4,36 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 #include "PlayershipPaperCharacter.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class ASTEROIDS_API APlayershipPaperCharacter : public APaperCharacter
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+public:
+    APlayershipPaperCharacter();
+
+private:
+    int playerMoveSpeed;
+    int playerRotateSpeed;
+
+protected:
+    virtual void BeginPlay() override;
+
+    void Move();
+    void Rotate();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+    UInputMappingContext* DefaultMappingContext;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+    UInputAction* MoveAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+    UInputAction* RotateAction;
 };
