704a1e6fa3570669b411781b332d028f99064b50
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-05-26 15:20:51 -0300

    initial commit

---
+++ Source/AsteroidsEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AsteroidsEditorTarget : TargetRules
+{
+	public AsteroidsEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("Asteroids");
+	}
+}
