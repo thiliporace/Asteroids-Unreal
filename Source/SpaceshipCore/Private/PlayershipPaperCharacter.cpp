f3966e336ab399da098a06d356585a0ef0cfaee7
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-05-28 16:36:26 -0300

    comecar a fazer movimentacao do jogador

--- Source/SpaceshipCore/Private/PlayershipPaperCharacter.cpp
+++ Source/SpaceshipCore/Private/PlayershipPaperCharacter.cpp
@@ -3,16 +3,16 @@
 
 #include "PlayershipPaperCharacter.h"
 
-APlayershipPaperCharacter::APlayershipPaperCharacter(): playerMoveSpeed(5), playerRotateSpeed(3) {
+#include <iostream>
+#include <ostream>
+
+APlayershipPaperCharacter::APlayershipPaperCharacter(): playerMoveSpeed(MAX_PLAYER_MOVE_SPEED), playerRotateSpeed(MAX_PLAYER_ROTATE_SPEED), rotateAmount(0) {
 	PrimaryActorTick.bCanEverTick = true;
-	UE_LOG(LogTemp, Warning, TEXT("Construto"));
 }
 
 void APlayershipPaperCharacter::BeginPlay() {
 	//Garante que o codigo da classe base tambem e chamado
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Begin plei"));
 
 	//Pegar o subsistema do Enhanced Input
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller)) {
@@ -23,6 +23,8 @@ void APlayershipPaperCharacter::BeginPlay() {
 		else {
 			UE_LOG(LogTemp, Warning, TEXT("Nao conseguiu criar Mapping Context"));
 		}
+
+		SetupPlayerInputComponent(PlayerController->InputComponent);
 	}
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("Nao conseguiu achar o Player Controller"));
@@ -31,19 +33,39 @@ void APlayershipPaperCharacter::BeginPlay() {
 
 void APlayershipPaperCharacter::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("Ticking..."));
 }
 
 void APlayershipPaperCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) {
-	
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayershipPaperCharacter::Move);
+		EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered, this, &APlayershipPaperCharacter::Rotate);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Nao conseguiu setar InputComponent do Player"));
+	}
 }
 
 void APlayershipPaperCharacter::Move(const FInputActionValue& Value) {
-
+	const FVector upVector = GetActorUpVector();
+	float movementAmount = Value.Get<float>();
+	
+	AddMovementInput(upVector, movementAmount * playerMoveSpeed); //Faltando deltaTime (?)
+	
+	UE_LOG(LogTemp, Log, TEXT("%f"),movementAmount * playerRotateSpeed);
 }
 
 void APlayershipPaperCharacter::Rotate(const FInputActionValue& Value) {
+	float rotateDirection = Value.Get<float>();
+	//UE_LOG(LogTemp, Log, TEXT("%f"),rotateDirection);
+	
+	rotateAmount += rotateDirection * playerRotateSpeed;
+	
+	FRotator newRotation = FRotator(GetControlRotation().Pitch, rotateAmount, GetControlRotation().Yaw); //Faltando deltaTime (?)
+	UE_LOG(LogTemp, Log, TEXT("%f"),newRotation.Yaw);
 
+	UCapsuleComponent* capsuleComponent = GetCapsuleComponent();
+	capsuleComponent->SetWorldRotation(newRotation);
+	SetActorRotation(newRotation);
 }
 
 
--- Source/SpaceshipCore/Public/PlayershipPaperCharacter.h
+++ Source/SpaceshipCore/Public/PlayershipPaperCharacter.h
@@ -8,11 +8,16 @@
 #include "InputMappingContext.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+
+//Tem que ficar por ultimo
 #include "PlayershipPaperCharacter.generated.h"
 
 /**
  *
  */
+
 UCLASS()
 class SPACESHIPCORE_API APlayershipPaperCharacter : public APaperCharacter
 {
@@ -21,16 +26,19 @@ public:
     APlayershipPaperCharacter();
 
 private:
-    int playerMoveSpeed;
-    int playerRotateSpeed;
+    const int MAX_PLAYER_MOVE_SPEED = 5;
+    const int MAX_PLAYER_ROTATE_SPEED = 3;
+    
+    int playerMoveSpeed, playerRotateSpeed;
 
+    float rotateAmount;
 protected:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
     void Move(const FInputActionValue& Value);
-
+    
     void Rotate(const FInputActionValue& Value);
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
